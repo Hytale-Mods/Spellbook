1b687fa89e750008adf9a77c9056c965e2402bbb
Tyler Hancock <darklime@live.ca>
2026-01-18 23:32:43 -0700

    Update for compatibility with 2026.01.17

--- src/main/java/net/darkhax/spellbook/api/interaction/WarpHomeInteraction.java
+++ src/main/java/net/darkhax/spellbook/api/interaction/WarpHomeInteraction.java
@@ -1,18 +1,15 @@
 package net.darkhax.spellbook.api.interaction;
 
+import com.hypixel.hytale.builtin.teleport.components.TeleportHistory;
 import com.hypixel.hytale.codec.builder.BuilderCodec;
 import com.hypixel.hytale.component.CommandBuffer;
-import com.hypixel.hytale.component.ComponentAccessor;
 import com.hypixel.hytale.component.Ref;
-import com.hypixel.hytale.math.vector.Transform;
 import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.math.vector.Vector3f;
 import com.hypixel.hytale.protocol.InteractionType;
-import com.hypixel.hytale.server.core.entity.EntityUtils;
 import com.hypixel.hytale.server.core.entity.InteractionContext;
 import com.hypixel.hytale.server.core.entity.entities.Player;
-import com.hypixel.hytale.server.core.entity.entities.player.data.PlayerConfigData;
-import com.hypixel.hytale.server.core.entity.entities.player.data.PlayerRespawnPointData;
+import com.hypixel.hytale.server.core.modules.entity.component.HeadRotation;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.modules.entity.teleport.Teleport;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
@@ -21,8 +18,6 @@ import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 import javax.annotation.Nonnull;
-import java.util.Arrays;
-import java.util.Optional;
 
 public class WarpHomeInteraction extends SimpleInstantInteraction {
 
@@ -30,35 +25,20 @@ public class WarpHomeInteraction extends SimpleInstantInteraction {
 
     @Override
     protected void firstRun(@Nonnull InteractionType type, @Nonnull InteractionContext context, @Nonnull CooldownHandler cooldown) {
-        final Ref<EntityStore> ref = context.getEntity();
-        final CommandBuffer<EntityStore> commandBuffer = context.getCommandBuffer();
-        if (EntityUtils.getEntity(ref, commandBuffer) instanceof Player player) {
-            final Transform transform = getClosestRespawnPoint(player, commandBuffer);
+        final CommandBuffer<EntityStore> buffer = context.getCommandBuffer();
+        if (buffer != null) {
+            final Ref<EntityStore> ref = context.getEntity();
+            final World world = buffer.getExternalData().getWorld();
+            final TransformComponent transform = buffer.getComponent(ref, TransformComponent.getComponentType());
             if (transform != null) {
-                commandBuffer.addComponent(player.getReference(), Teleport.getComponentType(), new Teleport(null, transform));
+                final HeadRotation headRotation = buffer.getComponent(ref, HeadRotation.getComponentType());
+                if (headRotation != null) {
+                    final Vector3d oldPos = transform.getPosition().clone();
+                    final Vector3f oldRotation = headRotation.getRotation().clone();
+                    buffer.ensureAndGetComponent(ref, TeleportHistory.getComponentType()).append(world, oldPos, oldRotation, "Home");
+                    buffer.addComponent(ref, Teleport.getComponentType(), Teleport.createForPlayer(null, Player.getRespawnPosition(ref, world.getName(), buffer)));
+                }
             }
         }
-    }
-
-    private static Transform getClosestRespawnPoint(Player player, ComponentAccessor<EntityStore> componentAccessor) {
-        final World world = player.getWorld();
-        final Ref<EntityStore> ref = player.getReference();
-        final PlayerConfigData playerData = player.getPlayerConfigData();
-        final PlayerRespawnPointData[] respawnPoints = playerData.getPerWorldData(world.getName()).getRespawnPoints();
-        if (respawnPoints != null && respawnPoints.length != 0) {
-            final TransformComponent playerTransformPos = componentAccessor.getComponent(ref, TransformComponent.getComponentType());
-            if (playerTransformPos != null) {
-                final Vector3d playerPos = playerTransformPos.getPosition();
-                final Optional<PlayerRespawnPointData> nearestPos = Arrays.stream(respawnPoints).min((a, b) -> {
-                    final Vector3d posA = a.getRespawnPosition();
-                    final Vector3d posB = b.getRespawnPosition();
-                    return Double.compare(playerPos.distanceSquaredTo(posA.x, playerPos.y, posA.z), playerPos.distanceSquaredTo(posB.x, playerPos.y, posB.z));
-                });
-                return new Transform((nearestPos.get()).getRespawnPosition());
-            }
-        }
-        final Transform worldSpawnPoint = world.getWorldConfig().getSpawnProvider().getSpawnPoint(ref, componentAccessor);
-        worldSpawnPoint.setRotation(Vector3f.ZERO);
-        return worldSpawnPoint;
     }
 }
